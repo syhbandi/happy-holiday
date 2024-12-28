985df575632e4cf77a5852579578e7da7872a2a9
syhbandi <saboonlbk@gmail.com>
2024-12-28 17:21:52 +0800

    add package controller

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -4,4 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PackageController extends Controller {}
+class PackageController extends Controller
+{
+  public function index()
+  {
+    return view('admin.packages');
+  }
+}
