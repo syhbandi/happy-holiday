4ab2e13ac170a8ca78c499f7d1f73c43add01d9a
syhbandi <saboonlbk@gmail.com>
2024-12-29 06:03:11 +0800

    add slug field to packages and sub_packages tables;

--- database/migrations/2024_12_28_063744_create_packages_table.php
+++ database/migrations/2024_12_28_063744_create_packages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('packages', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
+            $table->string('slug', 255);
             $table->string('description', 255);
             $table->string('price', 255);
             $table->text('image');
--- database/migrations/2024_12_28_063857_create_sub_packages_table.php
+++ database/migrations/2024_12_28_063857_create_sub_packages_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('package_id')->constrained()->onDelete('cascade');
             $table->string('name', 255);
+            $table->string('slug', 255);
             $table->string('description', 255);
             $table->string('price', 255);
             $table->text('image');
