91083b32dc32c321686fe6b625b32f31fafacf86
syhbandi <saboonlbk@gmail.com>
2024-12-28 17:23:09 +0800

    add footer component for admin

---
+++ resources/views/components/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="border-t border-neutral-200 bg-white">
+  <div class="container mx-auto flex items-center justify-between p-5">
+    <div class="text-sm text-neutral-600">
+      &copy; {{ date('Y') }} Happy Holiday All rights reserved.
+    </div>
+  </div>
