ac593c1868c21776b7c4bc0fafb00d351e7531af
syhbandi <saboonlbk@gmail.com>
2024-12-29 16:51:44 +0800

    feat: add error handling and success message display in package forms

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -3,13 +3,18 @@
   <x-admin.header>Tambah Paket</x-admin.header>
 
   <x-card class="w-full md:w-1/2">
-    <form action="/admin/packages" method="POST" enctype="multipart/form-data">
+    @error('status')
+      <div class="mb-5 rounded-lg border border-red-600 bg-red-100 p-4">{{ $message }}</div>
+    @enderror
+    <form action="/admin/packages" method="POST" enctype="multipart/form-data" x-data="{ isSubmitting: false }"
+      @submit.prevent="isSubmitting = true; $el.submit()">
       @csrf
       <div class="grid grid-cols-1 gap-3">
-        <x-input id="name" name="name" type="text" label="Nama Paket" />
-        <x-input id="description" name="desctiprion" type="textarea" label="Deskripsi" />
-        <x-input id="price" name="price" type="text" label="Harga" />
-        <x-input id="image" name="image" type="file" label="Gambar" />
+        <x-input id="name" name="name" type="text" value="{{ old('name') }}" label="Nama Paket" />
+        <x-input id="description" name="description" type="textarea" value="{{ old('description') }}"
+          label="Deskripsi" />
+        <x-input id="price" name="price" type="text" value="{{ old('price') }}" label="Harga" />
+        <x-input id="image" name="image" type="file" value="{{ old('image') }}" label="Gambar" />
         <div class="mt-3 flex items-center justify-between">
           <a class="flex h-11 items-center justify-center gap-2 rounded-lg border border-neutral-200 px-4 text-primary hover:bg-neutral-100"
             href="/admin/packages" role="button">
@@ -17,9 +22,8 @@
             Batal
           </a>
           <button
-            class="flex h-11 items-center justify-center gap-2 rounded-lg border border-primary bg-primary px-4 text-white hover:bg-opacity-90"
-            type="submit">
-            Simpan
+            class="flex h-11 items-center justify-center gap-2 rounded-lg border border-primary bg-primary px-4 text-white hover:bg-opacity-90 disabled:cursor-not-allowed disabled:opacity-50"
+            type="submit" :disabled="isSubmitting" x-text="isSubmitting ? 'Proses...':'Simpan'">
           </button>
         </div>
       </div>
--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -5,19 +5,31 @@
       href="/admin/packages/create">Tambah
       Paket <ion-icon class="ml-2 text-xl" name="add-outline"></ion-icon></a>
   </div>
-  <x-card>
-    <div class="relative flex flex-wrap gap-5">
-      <img class="h-28 w-28 flex-1 rounded-lg object-cover md:shrink"
-        src="{{ asset('images/d4d73ip-3e04e1e8-4d81-4d03-ad64-88511ad47860-mjE5LO27NRTV4bg2.avif') }}" alt="paket tour" />
-      <div>
-        <h2 class="mb-2 text-xl font-semibold">Tour Lombok 2 hari 1 malam</h2>
-        <p class="mb-2 text-neutral-500">sequi pariatur! Obcaecati similique voluptas, nesciunt veritatis soluta quod
-          harum incidunt
-          consequuntur.</p>
-        <p class="font-semibold">{{ Number::currency(254000, in: 'IDR', locale: 'id') }}</p>
-        <button class="flex items-center text-sm font-medium text-primary">2 paket <ion-icon class="ml-2"
-            name="chevron-down-outline"></ion-icon></button>
-      </div>
+
+  @if (session()->has('success'))
+    <div class="rounded-lg border border-green-600 bg-green-100 p-5 text-green-600">
+      {{ session('success') }}
     </div>
-  </x-card>
+  @endif
+
+  <div class="space-y-3">
+    @foreach ($packages as $package)
+      <x-card>
+        <div class="relative flex flex-wrap gap-5">
+          <img class="h-28 w-full rounded-lg object-cover md:w-28"
+            src="{{ $package->image ? Storage::url($package->image) : asset('images/sunrise-gili-trawangan.webp') }}"
+            alt="paket tour" />
+          <div>
+            <h2 class="mb-2 text-xl font-semibold">{{ $package->name }}</h2>
+            <p class="mb-2 text-neutral-500">{{ Str::limit($package->description, 100) }}</p>
+            <p class="font-semibold">{{ Number::currency($package->price, in: 'IDR', locale: 'id') }}</p>
+            <button class="flex items-center text-sm font-medium text-primary">2 paket <ion-icon class="ml-2"
+                name="chevron-down-outline"></ion-icon></button>
+          </div>
+        </div>
+      </x-card>
+    @endforeach
+    {{ $packages->links() }}
+  </div>
+
 </x-admin.layout>
