f31dd7c5782973aad8203bdac09d736248f10e29
syhbandi <saboonlbk@gmail.com>
2024-12-29 08:17:48 +0800

    refactor: replace HTML structure with Blade components for admin login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Admin</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js" defer></script>
-  </head>
-
-  <body class="bg-neutral-200 font-poppins">
+<x-layout>
+  <x-slot:title>Admin Login</x-slot>
+  <section class="bg-neutral-200">
     <div class="container mx-auto max-w-6xl px-5">
       <div class="flex h-screen items-center justify-center">
         <div class="w-full max-w-sm">
@@ -51,6 +41,6 @@
           </div>
         </div>
       </div>
-  </body>
-
-</html>
+    </div>
+  </section>
+</x-layout>
