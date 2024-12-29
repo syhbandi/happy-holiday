9a92a201dc1e19cff98f6bf99d838a9cc64185de
syhbandi <saboonlbk@gmail.com>
2024-12-29 08:17:31 +0800

    fix: update title rendering to handle null values correctly

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title ? $title . ' - ' : '' }} Happy Holiday</title>
+    <title>{{ $title ?? '' }} Happy Holiday</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js" defer></script>
   </head>
