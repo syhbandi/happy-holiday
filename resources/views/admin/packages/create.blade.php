8c578b1bded551e5fe76dea91b3b003c957161f2
syhbandi <saboonlbk@gmail.com>
2024-12-29 10:06:34 +0800

    refactor: simplify package creation form layout and improve input components

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -2,46 +2,26 @@
   <x-slot:title>Tambah Paket Tour</x-slot>
   <x-admin.header>Tambah Paket</x-admin.header>
 
-  <x-card>
+  <x-card class="w-full md:w-1/2">
     <form action="/admin/packages" method="POST" enctype="multipart/form-data">
       @csrf
-      <div class="grid grid-cols-2 gap-5">
-        <div>
-          <label class="block text-sm font-medium text-gray-700" for="name">Nama Paket</label>
-          <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm" id="name" name="name"
-            type="text" value="{{ old('name') }}">
-          @error('name')
-            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-          @enderror
+      <div class="grid grid-cols-1 gap-3">
+        <x-input id="name" name="name" type="text" label="Nama Paket" />
+        <x-input id="description" name="desctiprion" type="textarea" label="Deskripsi" />
+        <x-input id="price" name="price" type="text" label="Harga" />
+        <x-input id="image" name="image" type="file" label="Gambar" />
+        <div class="mt-3 flex items-center justify-between">
+          <a class="flex h-11 items-center justify-center gap-2 rounded-lg border border-neutral-200 px-4 text-primary hover:bg-neutral-100"
+            href="/admin/packages" role="button">
+            <ion-icon class="text-lg" name="arrow-back-outline"></ion-icon>
+            Batal
+          </a>
+          <button
+            class="flex h-11 items-center justify-center gap-2 rounded-lg border border-primary bg-primary px-4 text-white hover:bg-opacity-90"
+            type="submit">
+            Simpan
+          </button>
         </div>
-        <div>
-          <label class="block text-sm font-medium text-gray-700" for="price">Harga Paket</label>
-          <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm" id="price" name="price"
-            type="text" value="{{ old('price') }}">
-          @error('price')
-            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-          @enderror
-        </div>
-        <div>
-          <label class="block text-sm font-medium text-gray-700" for="image">Gambar</label>
-          <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm" id="image" name="image"
-            type="file">
-          @error('image')
-            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-          @enderror
-        </div>
-        <div>
-          <label class="block text-sm font-medium text-gray-700" for="description">Deskripsi</label>
-          <textarea class="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm" id="description" name="description">{{ old('description') }}</textarea>
-          @error('description')
-            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-          @enderror
-        </div>
-      </div>
-      <div class="mt-5">
-        <button
-          class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          type="submit">Simpan</button>
       </div>
     </form>
   </x-card>
