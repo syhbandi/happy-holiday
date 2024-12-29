9cd6803b5afdecbd55eda93bd850db738fd685da
syhbandi <saboonlbk@gmail.com>
2024-12-29 08:18:23 +0800

    refactor footer component: update contact information and enhance link accessibility

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,45 +1,53 @@
 <footer class="bg-gray-800 py-12 text-white">
   <div class="container mx-auto max-w-6xl px-6">
     <div class="mb-5 grid grid-cols-1 gap-5 md:grid-cols-4">
-      <div>
+      <a href="/">
         <img class="h-24" src="{{ asset('android-chrome-512x512.png') }}" alt="happy holiday logo">
         <h1 class="text-2xl font-bold text-white">Happy Holiday</h1>
-      </div>
+      </a>
       <div>
         <h2 class="mb-5 text-xl font-bold">Menu</h2>
         <ul class="flex flex-col gap-3 font-medium text-white">
-          <li><a class="hover:text-primary" href="/">Home</a></li>
-          <li><a class="hover:text-primary" href="/packages">Paket Tour</a></li>
-          <li><a class="hover:text-primary" href="/about">Tentang Kami</a></li>
-          <li><a class="hover:text-primary" href="/contact">Kontak</a></li>
+          <li><a class="hover:underline" href="/">Home</a></li>
+          <li><a class="hover:underline" href="/packages">Paket Tour</a></li>
+          <li><a class="hover:underline" href="/about">Tentang Kami</a></li>
+          <li><a class="hover:underline" href="/contact">Kontak</a></li>
         </ul>
       </div>
       <div class="">
         <h2 class="mb-5 text-xl font-bold">Kontak</h2>
-        <div class="mb-3 flex flex-wrap gap-3">
+        <a class="mb-3 flex flex-wrap gap-3 hover:underline" href="https://maps.app.goo.gl/xTThap87rq6UnSyB6"
+          target="_blank">
           <ion-icon class="text-xl text-secondary" name="location-outline"></ion-icon>
-          <p class="flex-1">Jl. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+          <p class="flex-1">Jl. Baiturrahman gang Harum No. 2 Mataram, Nusa Tenggara Barat, Indonesia</p>
+        </a>
         <div class="mb-3 flex flex-wrap gap-3">
-          <ion-icon class="text-xl text-secondary" name="call-outline"></ion-icon>
-          <p class="flex-1">0812-3456-7890</p>
+          <ion-icon class="text-xl text-secondary" name="logo-whatsapp"></ion-icon>
+          <div class="flex flex-col">
+            <a class="hover:underline" href="https://wa.me/62895384297876" target="_blank">
+              0895-3842-97876
+            </a>
+            <a class="hover:underline" href="https://wa.me/6287743040833" target="_blank">
+              0877-4304-0833
+            </a>
+          </div>
         </div>
-        <div class="mb-3 flex flex-wrap gap-3">
+        <a class="mb-3 flex flex-wrap gap-3 hover:underline" href="mailto:abahalkaff56@gmail.com">
           <ion-icon class="text-xl text-secondary" name="mail-outline"></ion-icon>
-          <p class="flex-1">happyholiday@gmail.com</p>
-        </div>
+          <p class="flex-1">abahalkaff56@gmail.com</p>
+        </a>
       </div>
       <div>
         <h2 class="mb-5 text-xl font-bold">Sosial Media</h2>
         <div class="flex gap-5">
-          <a href="">
+          <a href="https://www.facebook.com/happyholiday" target="_blank">
             <ion-icon class="text-2xl text-secondary" name="logo-facebook"></ion-icon>
           </a>
-          <a href="">
+          <a href="https://www.instagram.com/happyholyyday" target="_blank">
             <ion-icon class="text-2xl text-secondary" name="logo-instagram"></ion-icon>
           </a>
-          <a href="">
-            <ion-icon class="text-2xl text-secondary" name="logo-twitter"></ion-icon>
+          <a href="https://tiktok.com/@happyholliday__" target="_blank">
+            <ion-icon class="text-2xl text-secondary" name="logo-tiktok"></ion-icon>
           </a>
         </div>
       </div>
