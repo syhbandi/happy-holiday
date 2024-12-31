acfc586758ce05dbccac47a6eb1f1f11c2d2c17f
syhbandi <saboonlbk@gmail.com>
2024-12-31 17:35:08 +0800

    feat: add meta tags for description, keywords, and author in layout components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="{{ $description ?? '' }}" />
+    <meta name="keywords" content="{{ $keywords ?? '' }}" />
+    <meta name="author" content="Happy Holiday" />
     <title>{{ $title ?? '' }} Happy Holiday</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js" defer></script>
