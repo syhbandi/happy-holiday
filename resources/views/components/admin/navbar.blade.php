a43449c4120662ab5c33a7238310ac9a3c13cc8f
syhbandi <saboonlbk@gmail.com>
2024-12-31 21:26:55 +0800

    feat: enhance admin navbar with dropdown for user profile and logout options

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -2,7 +2,18 @@
   <button x-on:click="toggle">
     <ion-icon class="text-xl" name="menu-outline"></ion-icon>
   </button>
-  <div>
-    <div>{{ Auth::user()->name }}</div>
+  <div class="relative py-3" x-data="{ show: false }">
+    <button class="flex items-center justify-center gap-2" x-on:click="show=!show">
+      <p>{{ Auth::user()->name }}</p>
+      <ion-icon name="caret-down-outline"></ion-icon>
+    </button>
+    <div class="absolute right-0 top-full min-w-full overflow-hidden rounded-lg bg-white drop-shadow-md"
+      x-on:click.outside="show=false" x-cloak x-show="show" x-transition.duration.300ms>
+      <a class="flex h-10 items-center gap-3 px-5 hover:bg-neutral-100" href="/admin/profile" role="button"><ion-icon
+          name="person-outline"></ion-icon>Profil</a>
+      <a class="flex h-10 items-center gap-3 px-5 hover:bg-neutral-100" href="/admin/logout" role="button"><ion-icon
+          name="log-out-outline"></ion-icon>Logout</a>
+    </div>
+
   </div>
 </nav>
