ca0385debf805289baa21c34c432c395c89af3f8
syhbandi <saboonlbk@gmail.com>
2024-12-31 17:43:35 +0800

    feat: update SEO meta tags and add Google Tag Manager integration

--- resources/views/components/client-layout.blade.php
+++ resources/views/components/client-layout.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
   <x-slot:title>{{ $title ?? '' }}</x-slot>
   <x-slot:description>{{ $description ?? 'Nikmasi wisata di lombok bersama Happy Holiday' }}</x-slot>
-  <x-slot:keywords>{{ $keywords ?? 'lombok, happy holiday, tour, travel, adventure, culture' }}</x-slot>
+  <x-slot:keywords>{{ $keywords ?? 'lombok, happy holiday, tour, travel, adventure, culture, wisata, budaya, destinasi' }}</x-slot>
   <x-navbar />
   {{ $slot }}
   <x-contact-button />
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,13 +8,40 @@
     <meta name="description" content="{{ $description ?? '' }}" />
     <meta name="keywords" content="{{ $keywords ?? '' }}" />
     <meta name="author" content="Happy Holiday" />
+    <meta property="og:title" content="{{ $title ?? '' }}">
+    <meta property="og:description" content="{{ $description ?? '' }}">
+    <meta property="og:image" content="{{ asset('android-chrome-512x512.png') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
     <title>{{ $title ?? '' }} Happy Holiday</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js" defer></script>
     {{ $scripts ?? '' }}
+    <!-- Google Tag Manager -->
+    <script>
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-PG6D89SL');
+    </script>
+    <!-- End Google Tag Manager -->
   </head>
 
   <body class="font-poppins">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG6D89SL"
+        style="display:none;visibility:hidden" height="0" width="0"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     {{ $slot }}
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
